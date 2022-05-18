6650306508515d0b16ea8729e8a5a1abc994fd95
PamelaMayen <73257728+PamelaMayen@users.noreply.github.com>
2022-05-18 15:52:36 -0600

    Add files via upload

---
+++ FransfordSystem/Models/Descripcion.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FransfordSystem.Models
+{
+    public class Descripcion
+    {
+        [Key]
+        [Required]
+        public int idDescripcion { get; set; }
+
+        [Required]
+        public string idExamen { get; set; }
+        public virtual Examen examen { get; set; }
+
+        [Required(ErrorMessage = "Debe de ingresar la descripción del examen")]
+        [StringLength(50, ErrorMessage = "No puede escribir más de 50 caracteres")]
+        [Display(Name = "Descripcion del examen")]
+        public string descripcionExamen { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]{2}$", ErrorMessage = "Valor invalido, debe ser de 0 a 15")]
+        public int valorMinimo { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]{2}$", ErrorMessage = "Valor invalido, debe ser de 0 a 15")]
+        public int valorMaximo { get; set; }
+    }
+}
---
+++ FransfordSystem/Models/Examen.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FransfordSystem.Models
+{
+    public class Examen
+    {
+        [Key]
+        [Required]
+        public int idExamen { get; set; }
+
+        /*
+        [Required]
+        public string idCategoria { get; set; }
+        public virtual Categoria categoria { get; set; }
+        */
+
+        [Display(Name = "Nombre de examen")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"[ A-Za-zäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙÑñ.-]+", ErrorMessage = "Ingrese un nombre valido")]
+        public string nombreExamen { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]{3}.[0-9]{2}$", ErrorMessage = "")]
+        public float PrecioExamen { get; set; }
+    }
+}
