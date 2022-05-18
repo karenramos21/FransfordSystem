3ff4b0caec7a7861a3469c6ac9df2d8a5ffd29d3
Abner <mz17006@ues.edu.sv>
2022-05-17 22:10:23 -0600

    Agregar archivos de proyecto.

---
+++ FransfordSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FransfordSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
