e77dd1b52701baad970f1b989917a58f217bbceb
Abner <mz17006@ues.edu.sv>
2022-05-24 00:17:00 -0600

    Ya agregado Login

--- FransfordSystem/FransforDbContext.cs
+++ FransfordSystem/FransforDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using FransfordSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 
 //Clase que sirve para conectar a la base integrando las tablas en models
 namespace FransfordSystem
 {
-    public class FransforDbContext : DbContext
+    public class FransforDbContext : IdentityDbContext<Usuario>
     {
         public FransforDbContext(DbContextOptions<FransforDbContext> options)
             : base(options) { 
@@ -19,12 +20,15 @@ namespace FransfordSystem
         public DbSet<Usuario> Usuario { get; set; }
         public DbSet<Cliente> Cliente { get; set; }
         public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Examen> Examen { get; set; }
+        public DbSet<Descripcion> Descripcion { get; set; }
+
 
 
 
 
 
     }
-     
+
 
 }
--- FransfordSystem/Models/Usuario.cs
+++ FransfordSystem/Models/Usuario.cs
@@ -1,27 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace FransfordSystem.Models
 {
-    public class Usuario
+    public class Usuario : IdentityUser 
     {
-        [Display(Name = "idUsuario")]
-        [Required(ErrorMessage = "Este campo es obligatorio")]
-        [Key]
-        public int IdUsuario { get; set; }
-
-        [Display(Name = "Nombres del Usuario")]
-        [RegularExpression(@"[ A-Za-zäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙÑñ.-]+", ErrorMessage = "Ingrese nombres validos")]
-        [Required(ErrorMessage = "Este campo es obligatorio")]
-        public string nombreUsuario { get; set; }
-
-        [Display(Name = "Contrase;a")]
-        [RegularExpression(@"[ A-Za-zäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙÑñ.-]+", ErrorMessage = "ingrese apellidos validos")]
-        [Required(ErrorMessage = "Este campo es obligatorio")]
-        public string contrasenia { get; set; }
-
-
-
-
-
     }
 }
